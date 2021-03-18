871fa9d62b10446f003cf9c7f467f7ac9356955b
Inga-Helgadottitr <78418864+Inga-Helgadottitr@users.noreply.github.com>
2021-03-18 15:31:28 +0100

    Update DoVat.java

--- Opgave2/src/com/company/DoVat.java
+++ Opgave2/src/com/company/DoVat.java
@@ -2,12 +2,13 @@ package com.company;
 
 public class DoVat {
     double number;
+    final static int PRCVAT = 25;
     public DoVat(double number) {
         this.number = number;
     }
 
     public static double doVAT(double number){
-        double VAT = number / 100 * Main.PRCVAT;
+        double VAT = number / 100 * PRCVAT;
         return VAT;
     }
 }
