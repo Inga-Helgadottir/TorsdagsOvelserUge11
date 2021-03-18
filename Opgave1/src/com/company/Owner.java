0826ee8b71fc3dc9ac318b512e81ede841a12b22
Inga-Helgadottitr <78418864+Inga-Helgadottitr@users.noreply.github.com>
2021-03-18 14:04:02 +0100

    Create Owner.java

---
+++ Opgave1/src/com/company/Owner.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Owner {
+    String name;
+
+    public Owner() {
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
